18e07a454c29a024858339c30c3b9613f70d4e18
Tobias Cohen <me@tobiascohen.com>
2009-11-26 23:11:15 +1100

    updated to 'def_when', using === to give case-like evaluation

--- missingno.rb
+++ missingno.rb
@@ -1,5 +1,5 @@
 class Module
-  def defs regexp, &method
+  def def_when regexp, &method
     missingno_init
     @missingno_chain << [regexp, method]
   end
@@ -12,7 +12,7 @@ class Module
 
     old_method_missing = method :method_missing if method_defined? :method_missing
     define_method :method_missing do |sym, *args, &block|
-      if item = chain.find { |item| sym.to_s.match(item[0]) }
+      if item = chain.find { |item| item[0] === sym.to_s }
         item[1].call *($~.to_a.slice(1..-1) + args), &block
       elsif defined? old_method_missing and old_method_missing.respond_to? :call
         old_method_missing.call sym, *args, &block
@@ -23,7 +23,7 @@ class Module
     
     old_respond_to = method :respond_to? if method_defined? :respond_to?
     define_method :respond_to? do |sym|
-      if chain.any? { |item| sym.to_s.match(item[0]) }
+      if chain.any? { |item| item[0] === sym.to_s }
         true
       elsif defined? old_respond_to and old_respond_to.respond_to? :call
         old_respond_to.call sym, *args, &block
